5dc7615795f04cccc8de0c83504df50de7171ada
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-01-17 13:25:21 +0100

    added basic flask app configuration

---
+++ blog_auth/app/__init__.py
@@ -0,0 +1,40 @@
+import os
+
+from flask import Flask
+from flask_session import Session
+from flask.cli import load_dotenv
+from flask_cors import CORS
+from .db import initialize_db
+from .auth import initialize_auth
+from .views import (
+    auth_bp,
+)
+
+
+def create_app(test_config=None):
+    """Factory to create the Flask application
+    :return: A `Flask` application instance
+    """
+    app = Flask(__name__)
+    load_dotenv(".env")
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SESSION_TYPE'] = 'sqlalchemy'
+    app.config["EXTERNAL_API_URL"] = os.environ.get("EXTERNAL_API_URL")
+    if test_config:
+        app.config.update(test_config)
+    _register_blueprints(app)
+    initialize_db(app)
+    initialize_auth(app)
+    Session(app)
+    CORS(app, supports_credentials=True)
+
+    with app.app_context():
+        db.create_all()
+
+    return app
+
+
+def _register_blueprints(app):
+    app.register_blueprint(auth_bp, url_prefix="/auth")
---
+++ blog_auth/app/auth.py
@@ -0,0 +1,17 @@
+from authlib.integrations.flask_client import OAuth
+
+
+oauth = OAuth()
+
+def initialize_auth(app):
+    # OAuth configuration
+    oauth.init_app(app)
+    oauth.register(
+        name='google',
+        client_id=os.environ.get('GOOGLE_CLIENT_ID'),
+        client_secret=os.environ.get('GOOGLE_CLIENT_SECRET'),
+        access_token_url='https://accounts.google.com/o/oauth2/token',
+        authorize_url='https://accounts.google.com/o/oauth2/auth',
+        api_base_url='https://www.googleapis.com/oauth2/v1/',
+        client_kwargs={'scope': 'openid email profile'}
+    )
