687217610645ea1a39363acf7d2cde7d5bc9ce00
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-01-17 16:25:12 +0100

    adapted flask migrate

--- blog_auth/app/db.py
+++ blog_auth/app/db.py
@@ -1,10 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 db = SQLAlchemy()
+migrate = None
 
 def initialize_db(app):
     db.init_app(app)
 
-    with app.app_context():
-        db.create_all()
+    global migrate
+    migrate = Migrate(app, db)
