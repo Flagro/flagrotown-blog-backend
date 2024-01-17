72266cb83188ed93b770b5661e8c29dba1174638
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-01-17 16:45:31 +0100

    allowed hosts check

--- blog_auth/app/__init__.py
+++ blog_auth/app/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from flask import request, abort
 from flask_session import Session
 from flask_cors import CORS
 from .db import initialize_db
@@ -8,13 +9,28 @@ from .auth import initialize_auth
 from .views.auth import auth_bp
 
 
+def check_allowed_hosts(app):
+    @app.before_request
+    def before_request():
+        host = request.headers.get('Host')
+        if host not in app.config['ALLOWED_HOSTS']:
+            abort(403)  # Forbidden access
+
+
 def create_app(test_config=None):
     """Factory to create the Flask application
     :return: A `Flask` application instance
     """
     app = Flask(__name__)
+
+    if os.environ.get('ENVIRONMENT') == 'development':
+        allowed_hosts = ['localhost', '127.0.0.1', '0.0.0.0']
+    else:
+        allowed_hosts = ['blog.flagrotown.com']
+
     app.config.from_mapping(
         SECRET_KEY=os.environ.get('SECRET_KEY'),
+        ALLOWED_HOSTS=allowed_hosts,
         SQLALCHEMY_DATABASE_URI="{}://{}:{}@{}:{}/{}".format(
             os.environ.get('DB_ENGINE'),
             os.environ.get('DB_USER'),
@@ -31,6 +47,7 @@ def create_app(test_config=None):
     _register_blueprints(app)
     initialize_db(app)
     initialize_auth(app)
+    check_allowed_hosts(app)
     Session(app)
     CORS(app, supports_credentials=True)
 
