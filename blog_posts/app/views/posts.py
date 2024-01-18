9407517c45c68cfe792ff3032f1f45cf5589e01c
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-01-19 00:20:33 +0100

    fixed posts route

--- blog_posts/app/views/posts.py
+++ blog_posts/app/views/posts.py
@@ -4,7 +4,7 @@ from ..models.post import Post
 
 posts_bp = Blueprint('posts_bp', __name__)
 
-@posts_bp.route('/posts/<int:post_id>', methods=['GET'])
+@posts_bp.route('/<int:post_id>', methods=['GET'])
 def get_post(post_id):
     post = Post.query.get(post_id)
     if not post:
