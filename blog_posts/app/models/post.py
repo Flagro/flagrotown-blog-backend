5178d8c3360076dda8b8015411a9a3c0a3cd5fce
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-01-19 00:09:52 +0100

    update_image_links method

--- blog_posts/app/models/post.py
+++ blog_posts/app/models/post.py
@@ -1,3 +1,4 @@
+import re
 from sqlalchemy.orm import relationship
 from ..db import db
 
@@ -11,3 +12,20 @@ class Post(db.Model):
 
     def __repr__(self):
         return f'<Post {self.id}>'
+
+    def update_image_links(self, object_storage):
+        """
+        Update Markdown image references in the post text with Object Storage URLs, 
+        while preserving the alt text.
+        """
+        pattern = r'(!\[.*?\])\(\.\./images/(.*?)\)'
+
+        def replace_with_url(match):
+            alt_text = match.group(1)
+            filename = match.group(2)
+            image = next((img for img in self.images if img.filename == filename), None)
+            if image:
+                return f'{alt_text}({object_storage.generate_file_url(image.filename)})'
+            return match.group(0)  # No replacement if no corresponding image
+
+        self.text = re.sub(pattern, replace_with_url, self.text)
