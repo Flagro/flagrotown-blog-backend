1b0ba4c7bdaebf88c5bbea6139f817ffbe3d7743
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-01-19 18:02:28 +0100

    new metrics view logic

--- blog_metrics/app/views/metrics.py
+++ blog_metrics/app/views/metrics.py
@@ -1,16 +1,28 @@
+import requests
 from flask import Blueprint, jsonify, abort
-from ..models.metrics import Metrics
+from ..models.metrics import PostAnalytics
+from ..db import db
 
 
 metrics_bp = Blueprint('metrics_bp', __name__)
 
+
+def check_post_exists(post_id):
+    response = requests.get(f'https://blog-api.flagrotown.com/posts/{post_id}')
+    return response.status_code == 200
+
+
 @metrics_bp.route('/<int:post_id>', methods=['GET'])
 def get_metrics(post_id):
-    metrics = Metrics.query.get(post_id)
+    if not check_post_exists(post_id):
+        abort(404, description=f"Post {post_id} doesn't exist")
+
+    metrics = PostAnalytics.query.get(post_id)
     if not metrics:
-        abort(404, description=f"Metrics for post with ID {post_id} not found")
+        metrics = PostAnalytics(post_id=post_id)
+        db.session.add(metrics)
+        db.session.commit()
 
     return jsonify({
-        'id': metrics.id,
-        'metrics': metrics.metrics
+        'views': metrics.views,
     })
