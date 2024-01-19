00146e893689a1820d0d47c2eb171525678f33d1
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-01-19 12:31:52 +0100

    included file contents with get diff

--- blog_posts/app/blog_repository.py
+++ blog_posts/app/blog_repository.py
@@ -1,4 +1,5 @@
 import requests
+import base64
 
 
 class BlogRepository:
@@ -22,11 +23,22 @@ class BlogRepository:
         response.raise_for_status()
         return response.json()['sha']
 
+    def _get_file_content(self, file_path):
+        url = f"https://api.github.com/repos/{self.owner}/{self.repo}/contents/{file_path}"
+        response = requests.get(url)
+        response.raise_for_status()
+        content_data = response.json()
+        if content_data['encoding'] == 'base64':
+            file_content = base64.b64decode(content_data['content'])
+        else:
+            raise ValueError("Unsupported file encoding")
+        return file_content
+
     def get_diff(self):
         latest_commit = self._get_latest_commit()
 
         added, deleted = self._compare_commits(latest_commit)
-
+                
         self.last_known_commit = latest_commit
         return added, deleted
 
@@ -36,7 +48,8 @@ class BlogRepository:
         response.raise_for_status()
         comparison_data = response.json()
 
-        added = [file['filename'] for file in comparison_data['files'] if file['status'] in ['added', 'modified']]
+        added_files = [file['filename'] for file in comparison_data['files'] if file['status'] in ['added', 'modified']]
+        added = [(file, self._get_file_content(file)) for file in added_files]
         deleted = [file['filename'] for file in comparison_data['files'] if file['status'] in ['removed', 'modified']]
         
         return added, deleted
@@ -46,4 +59,4 @@ blog_repository = BlogRepository()
 
 
 def initialize_blog_repository(app):
-    blog_repository.init_app(app)    
+    blog_repository.init_app(app)
